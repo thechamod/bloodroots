0e43dfdda027a33bf7a8a9fc22d4466e6d5f5005
thechamod <gimhanwijekoon5@gmail.com>
2025-05-06 21:03:59 +0530

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -13,7 +13,7 @@ const VideoPlayer = ({ videoId }) => {
           <div className="absolute inset-0 bg-red-900/20 z-0"></div>
           <iframe
             className="absolute inset-0 w-full h-full z-10"
-            src={`https://www.youtube.com/embed/KgOjCzBfuGA?si=xl0NEpoTTWVzfXF5`}
+            src={`https://www.youtube.com/embed/KgOjCzBfuGA?si=gU-oDxFn96W0MzdW`}
             title="Bloodroots Music Video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
