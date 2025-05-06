273baa0a077034bb4318871870f46921eb13fb1f
thechamod <gimhanwijekoon5@gmail.com>
2025-05-06 21:56:34 +0530

    Update ComingSoon.jsx

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -178,7 +178,7 @@ const ComingSoon = () => {
                 {/* Hidden audio element */}
                 <audio 
                   ref={audioRef} 
-                  src="#" 
+                  src="https://whyp.it/tracks/embed?id=278320" 
                   className="hidden"
                 ></audio>
                 
